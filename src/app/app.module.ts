7cbd84db666d89e569d149c3d483b41ffd6dfa46
Jeferson Fischer <cffischer100@gmail.com>
2021-08-28 14:48:37 -0300

    Projeto Base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ListaComponent } from './lista/lista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CadastroComponent,
+    ListaComponent
   ],
   imports: [
     BrowserModule,
